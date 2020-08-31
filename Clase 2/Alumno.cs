8b0b64051f08347079adc806b86daf10f736414a
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2020-08-31 18:12:46 -0300

    ejercicios clase 2

---
+++ Clase 2/Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_2
+{
+    class Alumno
+    {
+        public byte nota1;
+        public byte nota2;
+        public float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+
+        public void CalcularFinal()
+        {
+            Random random = new Random();
+
+            if (this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                this.notaFinal = (byte)random.Next(0,10);
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+                
+        }
+
+        public void Mostrar()
+        {
+            if (this.notaFinal != -1)
+            {
+                Console.WriteLine("Nombre: {0} Apellido: {1} Legajo: {2} Primer nota: {3} Segunda nota: {4} Nota final: {5} \n", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2, this.notaFinal);
+            }
+            else
+            {
+                Console.WriteLine("Nombre: {0} Apellido: {1} Legajo: {2} Primer nota: {3} Segunda nota: {4} Nota final: Alumno desaprobado \n", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2);
+            }
+        }
+    }
+}
---
+++ Clase 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+
+            alumno1.nombre = "Quimey";
+            alumno2.nombre = "Santiago";
+            alumno3.nombre = "Alejo";
+            alumno1.apellido = "Espinosa";
+            alumno2.apellido = "Anllo";
+            alumno3.apellido = "Carmona";
+            alumno1.legajo = 1033;
+            alumno2.legajo = 1034;
+            alumno3.legajo = 1038;
+
+            alumno1.Estudiar(4, 6);
+            alumno2.Estudiar(5, 6);
+            alumno3.Estudiar(9, 10);
+
+            Console.Write("--- Notas alumnos ---\n\n");
+
+            alumno1.Mostrar();
+            alumno2.Mostrar();
+            alumno3.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
